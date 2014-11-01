aa336d10d89eca2e74a0e5ba924e4dd36f3b9de4
Neil Piper <n_piper@hotmail.com>
2014-11-01 22:03:01 +1100

    updated scripts

--- Vagrant/data/installJenkins.sh
+++ Vagrant/data/installJenkins.sh
@@ -5,4 +5,6 @@ dpkg-reconfigure --frontend noninteractive tzdata
 puppet module install puppetlabs-stdlib
 puppet module install puppetlabs-apt
 puppet module install puppetlabs-java
+puppet module install puppetlabs-git
+puppet module install maestrodev-maven
 puppet module install rtyler-jenkins
--- Vagrant/data/puppetScript.pp
+++ Vagrant/data/puppetScript.pp
@@ -1,15 +1,42 @@
 node 'ubuntu1310-i386' {
-    user {'jenkins':
-      ensure => absent,
-    }   
 
-  include jenkins
+include git
+include git::subtree
 
-  jenkins::plugin {
-  "git" : ;
-  "radiatorviewplugin" : ;
-  "jira" : ;
-  "confluencepublisher" : ;
-  "jenkins-cloudformation-plugin" : ;
-  }  
+git::config { 'user.name':
+    value => 'Neil Piper',
+}
+
+git::config { 'user.email':
+  value => 'n_piper@hotmail.com',
+}
+
+include jenkins 
+
+jenkins::plugin { 
+	'git': ;
+        'github': ;
+        'confluence-publisher': ;
+        'jira': ;
+        'radiatorviewplugin': ;
+        'chucknorris' : ;
+}
+
+ # defaults for all maven{} declarations
+Maven {
+  user  => "maven", # you can make puppet run Maven as a specific user instead of root, useful to share Maven settings and local repository
+  group => "maven", # you can make puppet run Maven as a specific group
+  repos => "http://repo.maven.apache.org/maven2"
+}
+
+ # Install Maven
+class { "maven::maven":
+  version => "3.0.5", # version to install
+  # you can get Maven tarball from a Maven repository instead than from Apache servers, optionally with a user/password
+  repo => {
+    #url => "http://repo.maven.apache.org/maven2",
+    #username => "",
+    #password => "",
+  }
+}
 }
